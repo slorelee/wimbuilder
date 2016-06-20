b16c077daf369c496ac1b8fa4d391a7723c591a0
slore <slorelee@gmail.com>
2016-06-20 22:14:43 +0800

    export image with  /Bootable option

--- PE_Builder/bin/WIM_Exporter.bat
+++ PE_Builder/bin/WIM_Exporter.bat
@@ -2,7 +2,7 @@ if "x%~1"=="x" goto :EOF
 set GetLastError=1
 if exist "%~dpn1_build%~x1" del /q "%~dpn1_build%~x1"
 call PB_LOG "[%BUILD_PROJECT%] --- EXPORT [%~1] -%%gt:%% [%~dpn1_build%~x1]"
-call DismX /Export-Image /SourceImageFile:"%~1" /SourceIndex:1 /DestinationImageFile:"%~dpn1_build%~x1"
+call DismX /Export-Image /SourceImageFile:"%~1" /SourceIndex:1 /DestinationImageFile:"%~dpn1_build%~x1" /Bootable
 if "%errorlevel%"=="0" (
   del /q "%~1"
   set GetLastError=0
