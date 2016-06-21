c291280515ec51da8eca38f849ef2b1035d77ace
lyy <li_yongyong@nec.cn>
2016-06-21 08:56:54 +0800

    use imagex for building on Windows 7

--- PE_Builder/bin/WIM_Exporter.bat
+++ PE_Builder/bin/WIM_Exporter.bat
@@ -2,7 +2,14 @@ if "x%~1"=="x" goto :EOF
 set GetLastError=1
 if exist "%~dpn1_build%~x1" del /q "%~dpn1_build%~x1"
 call PB_LOG "[%BUILD_PROJECT%] --- EXPORT [%~1:%PB_BASE_INDEX%] -%%gt:%% [%~dpn1_build%~x1]"
-call DismX /Export-Image /SourceImageFile:"%~1" /SourceIndex:%PB_BASE_INDEX% /DestinationImageFile:"%~dpn1_build%~x1" /Bootable
+
+rem use imagex for building on Windows 7
+ver|findstr " 6.1." >nul
+if not ERRORLEVEL 1 (
+  imagex_x86.exe /Export "%~1" %PB_BASE_INDEX% "%~dpn1_build%~x1" /boot
+) else (
+  call DismX /Export-Image /SourceImageFile:"%~1" /SourceIndex:%PB_BASE_INDEX% /DestinationImageFile:"%~dpn1_build%~x1" /Bootable
+)
 if "%errorlevel%"=="0" (
   del /q "%~1"
   set GetLastError=0
